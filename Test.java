9df840d9ebd6ff7a63ec02989be068be378bc722
Divyajyothi2210 <88102484+Divyajyothi2210@users.noreply.github.com>
2021-08-30 12:12:00 +0530

    ...

---
+++ Test.java
@@ -0,0 +1,16 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		AbstractApplicationContext con;
+		con =new ClassPathXmlApplicationContext("spring1.xml");
+		con.registerShutdownHook();
+		customer c= (customer) con.getBean("customer");
+		c.print();
+
+	}
+
+}
